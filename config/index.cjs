d7cf1d994b3d1bd608174458bff0068297d4f577
h030302 <117762188+h030302@users.noreply.github.com>
2022-11-09 13:45:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '*生日', name: '小志', year: '2003', date: '03-04',
+          type: '生日', name: '小志', year: '2003', date: '01-13',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '10-22',
